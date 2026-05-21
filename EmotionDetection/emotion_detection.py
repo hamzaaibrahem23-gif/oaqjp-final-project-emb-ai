1821e8d2ab5f0f2d183f7fd2c9f6d5dd7d260ffe
hamzaaibrahem23-gif <hamzaaibrahem23@gmail.com>
2026-05-22 00:34:34 +0300

    Implement emotion detection function
    Added a function to detect emotions from text using an external API.

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,1 +1,26 @@
+import requests
+import json
 
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+    request_object = {
+        "raw_document": {"text": text_to_analyze}
+    }
+    response = requests.post(url, json=request_object, headers=headers)
+
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        emotions = formatted_response["emotionPredictions"][0]["emotion"]
+
+    elif response.status_code == 500:
+        label = None
+        score = None
+    
+    else:
+        label = None
+        score = None
+
+    return emotions
